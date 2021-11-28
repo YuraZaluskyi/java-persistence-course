ce13c71ff2dcf6e4f0b1211938268a7a116ae9e4
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2021-11-28 18:37:41 +0200

    commit

--- 3-0-jpa-and-hibernate/3-0-0-hello-jpa-entity/src/main/java/com/bobocode/model/Movie.java
+++ 3-0-jpa-and-hibernate/3-0-0-hello-jpa-entity/src/main/java/com/bobocode/model/Movie.java
@@ -1,5 +1,12 @@
 package com.bobocode.model;
 
+import javax.annotation.Nonnull;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,12 +21,17 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "movie")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-
+    @Column(name = "name", nullable = false)
     private String name;
-
+    @Column(name = "director", nullable = false)
     private String director;
-
+    @Column(name = "duration")
     private Integer durationSeconds;
 }
